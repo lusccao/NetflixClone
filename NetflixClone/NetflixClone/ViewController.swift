b83698382836f4cbeb95c83d616f5e8caae56875
Lucas <larthur@webeleven.com.br>
2024-01-02 17:36:19 -0300

    Iniciando projeto netflix clone

---
+++ NetflixClone/NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Lucas Arthur on 02/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
